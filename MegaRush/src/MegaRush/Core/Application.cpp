c3838297486e4eedf564d95740e559231ebf5bcb
Aryan Sinha <aryananusinha@gmail.com>
2020-08-15 13:14:41 -0400

    Added test OpenGL code
    This acts as a test point.

--- MegaRush/src/MegaRush/Core/Application.cpp
+++ MegaRush/src/MegaRush/Core/Application.cpp
@@ -1,7 +1,48 @@
 #include "Application.h"
 
+#include <glad/glad.h>
+
 namespace MegaRush
 {
+	float vertices[] =
+	{
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.5f,  0.5f, 0.0f,
+		-0.5f,  0.5f, 0.0f
+	};
+
+	uint32_t indices[] =
+	{
+		0, 1, 2,
+		2, 3, 0
+	};
+
+	std::string vertexSource = R"(
+		#version 330 core
+
+		layout (location = 0) in vec3 aPos;
+		
+		void main()
+		{
+			gl_Position = vec4(aPos, 1.0);
+		}
+	)";
+
+	std::string fragmentSource = R"(
+		#version 330 core
+
+		out vec4 color;
+
+		void main()
+		{
+			color = vec4(0.2, 0.3, 0.8, 1.0);
+		}
+	)";
+
+	uint32_t vao, vbo, ebo;
+	uint32_t shaderProgram;
+
 	Application* Application::s_Instance = nullptr;
 
 	Application::Application(const char* title) : m_Running(false)
@@ -13,6 +54,64 @@ namespace MegaRush
 
 		m_RendererAPI = RendererAPI::Create();
 		m_RendererAPI->Init();
+
+		m_RendererAPI->SetClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
+
+		glCreateVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+
+		glCreateBuffers(1, &vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		
+		glCreateBuffers(1, &ebo);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+
+		uint32_t vertexShader, fragmentShader;
+		const char* vSourceCode = vertexSource.c_str();
+		const char* fSourceCode = fragmentSource.c_str();
+		vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+		int success;
+		char* infoLog = new char[512];
+		glShaderSource(vertexShader, 1, &vSourceCode, NULL);
+		glCompileShader(vertexShader);
+		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+			MR_CORE_ERROR("Failed to compile vertex shader: {0}", infoLog);
+		}
+
+		glShaderSource(fragmentShader, 1, &fSourceCode, NULL);
+		glCompileShader(fragmentShader);
+		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+			MR_CORE_ERROR("Failed to compile fragment shader: {0}", infoLog);
+		}
+
+		shaderProgram = glCreateProgram();
+
+		glAttachShader(shaderProgram, vertexShader);
+		glAttachShader(shaderProgram, fragmentShader);
+		glLinkProgram(shaderProgram);
+		glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+		if (!success)
+		{
+			glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+			MR_CORE_ERROR("Failed to link shader program: {0}", infoLog);
+		}
+
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
 
 		s_Instance = this;
 
@@ -28,6 +127,11 @@ namespace MegaRush
 	{
 		while (m_Running)
 		{
+			m_RendererAPI->Clear();
+			
+			glUseProgram(shaderProgram);
+			glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(uint32_t), GL_UNSIGNED_INT, 0);
+
 			m_Window->OnUpdate();
 		}
 	}
@@ -41,6 +145,7 @@ namespace MegaRush
 
 	bool Application::OnWindowResize(WindowResizeEvent& e)
 	{
+		m_RendererAPI->SetViewport(0, 0, e.GetWidth(), e.GetHeight());
 		MR_CORE_WARN("Resized window: {0} x {1}", e.GetWidth(), e.GetHeight());
 		return false;
 	}
